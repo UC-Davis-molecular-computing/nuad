e8cbb5a30110d924917f632816211df58c5ed7fa
Benjamin Lee <bnllee@ucdavis.edu>
2021-05-17 22:31:18 -0700

    Refactor tester strand constructor logic

--- dsd/test.py
+++ dsd/test.py
@@ -1,11 +1,53 @@
+from typing import Dict, List
 import unittest
 from dsd import constraints
-from dsd.constraints import Domain, _exterior_base_type_of_domain_3p_end, Strand, DomainPool, BasePairType, _domains_interned
+from dsd.constraints import Domain, _exterior_base_type_of_domain_3p_end, Strand, DomainPool, BasePairType
 
 POOL_1 = DomainPool('POOL_1', 1)
 POOL_2 = DomainPool('POOL_2', 2)
 POOL_5 = DomainPool('POOL_5', 5)
 POOL_13 = DomainPool('POOL_13', 13)
+
+_domain_pools: Dict[int, DomainPool] = {}
+
+
+def assign_domain_pool_of_size(length: int) -> DomainPool:
+    """Returns a DomainPool of given size
+
+    :param length: Size of DomainPool
+    :type length: int
+    :return: DomainPool
+    :rtype: DomainPool
+    """
+    if length in _domain_pools:
+        return _domain_pools[length]
+    else:
+        new_domain_pool = DomainPool(f'POOL_{length}', length)
+        _domain_pools[length] = new_domain_pool
+        return new_domain_pool
+
+
+def construct_strand(domain_names: List[str], domain_lengths: List[int]) -> Strand:
+    """Constructs a strand with given domain names and domain lengths.
+
+    :param domain_names: Names of the domain on the strand
+    :type domain_names: List[str]
+    :param domain_lengths: Lengths of the domain on the strand
+    :type domain_lengths: List[int]
+    :raises ValueError: If domain_names is not same size as domain_lengths
+    :return: Strand
+    :rtype: Strand
+    """
+    if len(domain_names) != len(domain_lengths):
+        raise ValueError(f'domain_names and domain_lengths need to contain same'
+                         f'number of elements but instead found that '
+                         f'domain_names contained {len(domain_names)} names '
+                         f'but domain_lengths contained {len(domain_lengths)} '
+                         f'lengths')
+    s: Strand = Strand(domain_names)
+    for (i, length) in enumerate(domain_lengths):
+        s.domains[i].pool = assign_domain_pool_of_size(length)
+    return s
 
 
 class TestExteriorBaseTypeOfDomain3PEnd(unittest.TestCase):
@@ -27,12 +69,8 @@ class TestExteriorBaseTypeOfDomain3PEnd(unittest.TestCase):
           [=============--==>
                  b*       a*
         """
-        top_strand = Strand(['a', 'b'])
-        bot_strand = Strand(['b*', 'a*'])
-        top_strand.domains[0].pool = POOL_2
-        top_strand.domains[1].pool = POOL_13
-        bot_strand.domains[0].pool = POOL_13
-        bot_strand.domains[1].pool = POOL_2
+        top_strand = construct_strand(['a', 'b'], [2, 13])
+        bot_strand = construct_strand(['b*', 'a*'], [13, 2])
 
         top_a = top_strand.address_of_domain(0)
         top_b = top_strand.address_of_domain(1)
@@ -62,15 +100,8 @@ class TestExteriorBaseTypeOfDomain3PEnd(unittest.TestCase):
             [=====--=--=====>
                c*   d    a*
         """
-        top_strand = Strand(['a', 'b', 'c'])
-        top_strand.domains[0].pool = POOL_5
-        top_strand.domains[1].pool = POOL_1
-        top_strand.domains[2].pool = POOL_5
-
-        bot_strand = Strand(['c*', 'd', 'a*'])
-        bot_strand.domains[0].pool = POOL_5
-        bot_strand.domains[1].pool = POOL_1
-        bot_strand.domains[2].pool = POOL_5
+        top_strand = construct_strand(['a', 'b', 'c'], [5, 1, 5])
+        bot_strand = construct_strand(['c*', 'd', 'a*'], [5, 1, 5])
 
         top_a = top_strand.address_of_domain(0)
         top_c = top_strand.address_of_domain(2)
