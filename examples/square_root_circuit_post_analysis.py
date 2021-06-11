6c15758d7937a76653f2edae927dc679932cdf5e
Benjamin Lee <bnllee@ucdavis.edu>
2021-06-10 18:26:14 -0700

    Print pfunc for all strands

--- examples/square_root_circuit_post_analysis.py
+++ examples/square_root_circuit_post_analysis.py
@@ -1,50 +1,65 @@
 import argparse
 from dataclasses import dataclass
-from typing import List
+from typing import List, Iterable
+from itertools import chain
+from functools import reduce
+from dsd.vienna_nupack import pfunc4
 
 
 @dataclass
-class SignalStrand:
+class Strand:
+    name: str
+    sequence: str
+
+
+@dataclass
+class SignalStrand(Strand):
+    name: str
     gate_3p: str
     gate_5p: str
     sequence: str
 
 
 @dataclass
-class FuelStrand:
+class FuelStrand(Strand):
+    name: str
     gate_3p: str
     sequence: str
 
 
 @dataclass
-class GateBaseStrand:
+class GateBaseStrand(Strand):
     gate: str
     sequence: str
+    name: str
 
 
 @dataclass
-class ThresholdBottomStrand:
+class ThresholdBottomStrand(Strand):
     input: str
     gate: str
     sequence: str
 
 
 @dataclass
-class ThresholdTopStrand:
+class ThresholdTopStrand(Strand):
     gate: str
     sequence: str
+    name: str
 
 
 @dataclass
-class ReporterBottomStrand:
+class ReporterBottomStrand(Strand):
     gate: str
     sequence: str
+    name: str
 
 
 @dataclass
-class ReporterTopStrand:
+class ReporterTopStrand(Strand):
     gate: str
     sequence: str
+    name: str
 
 
 signal_strands: List[SignalStrand] = []
@@ -71,40 +86,47 @@ def process_line(line: str) -> None:
         gate_3p = strand_name_split[1]
         gate_5p = strand_name_split[2]
         assert len(line_split) == 5
-        signal_strands.append(SignalStrand(gate_3p=gate_3p, gate_5p=gate_5p, sequence=sequence))
+        signal_strands.append(SignalStrand(gate_3p=gate_3p, gate_5p=gate_5p, sequence=sequence, name=strand_name))
     elif strand_name.startswith('fuel'):
         assert len(strand_name_split) == 2
         gate_3p = strand_name_split[1]
         assert len(line_split) == 5
-        fuel_strands.append(FuelStrand(gate_3p=gate_3p, sequence=sequence))
+        fuel_strands.append(FuelStrand(gate_3p=gate_3p, sequence=sequence, name=strand_name))
     elif strand_name.startswith('gate_base'):
         assert len(strand_name_split) == 3
         gate = strand_name_split[2]
         assert len(line_split) == 5
-        gate_base_strands.append(GateBaseStrand(gate=gate, sequence=sequence))
+        gate_base_strands.append(GateBaseStrand(gate=gate, sequence=sequence, name=strand_name))
     elif strand_name.startswith('threshold_bottom'):
         assert len(strand_name_split) == 4
         input = strand_name_split[2]
         gate = strand_name_split[3]
         assert len(line_split) == 5
-        threshold_bottom_strands.append(ThresholdBottomStrand(input=input, gate=gate, sequence=sequence))
+        threshold_bottom_strands.append(ThresholdBottomStrand(
+            input=input, gate=gate, sequence=sequence, name=strand_name))
     elif strand_name.startswith('threshold_top'):
         assert len(strand_name_split) == 3
         gate = strand_name_split[2]
         assert len(line_split) == 3
-        threshold_top_strands.append(ThresholdTopStrand(gate=gate, sequence=sequence))
+        threshold_top_strands.append(ThresholdTopStrand(gate=gate, sequence=sequence, name=strand_name))
     elif strand_name.startswith('reporter_bottom'):
         assert len(strand_name_split) == 3
         gate = strand_name_split[2]
         assert len(line_split) == 4
-        reporter_bottom_strands.append(ReporterBottomStrand(gate=gate, sequence=sequence))
+        reporter_bottom_strands.append(ReporterBottomStrand(gate=gate, sequence=sequence, name=strand_name))
     elif strand_name.startswith('reporter_top'):
         assert len(strand_name_split) == 3
         gate = strand_name_split[2]
         assert len(line_split) == 3
-        reporter_top_strands.append(ReporterTopStrand(gate=gate, sequence=sequence))
+        reporter_top_strands.append(ReporterTopStrand(gate=gate, sequence=sequence, name=strand_name))
     else:
         raise ValueError("Unexpected strand name")
+
+
+def strands() -> Iterable[Strand]:
+    return chain(
+        signal_strands, fuel_strands, gate_base_strands, threshold_bottom_strands, threshold_top_strands,
+        reporter_bottom_strands, reporter_top_strands)
 
 
 def main():
@@ -118,7 +140,14 @@ def main():
         for line in f:
             process_line(line)
 
-    print('Done processing')
+    print(f'Done processing {filename}')
+
+    print('Calculating pfunc...')
+
+    longest_strand_name_length = reduce(lambda acc, cur: max(acc, len(cur.name)), strands(), 0)
+    for strand in strands():
+        pfunc4_val = pfunc4(strand.sequence)
+        print(f'{strand.name: <{longest_strand_name_length}} | pfunc: {pfunc4_val}')
 
 
 if __name__ == '__main__':
