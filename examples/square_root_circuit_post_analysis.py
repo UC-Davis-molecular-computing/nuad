f63503fd6d36124e20e8bc3641f905da5272b75e
Benjamin Lee <bnllee@ucdavis.edu>
2021-06-10 23:14:24 -0700

    Print complex MFE and bp for signal_strand/fuel gate complexes

--- examples/square_root_circuit_post_analysis.py
+++ examples/square_root_circuit_post_analysis.py
@@ -1,9 +1,11 @@
 import argparse
 from dataclasses import dataclass
-from typing import List, Iterable
+from typing import List, Iterable, Dict
 from itertools import chain, combinations
 from functools import reduce
 from dsd.vienna_nupack import pfunc4, binding4
+from nupack import Model, Complex, ComplexSet, SetSpec, complex_analysis
+from nupack import Strand as NupackStrand
 
 
 @dataclass
@@ -146,6 +148,37 @@ def calculate_and_print_binding(strand1: Strand, strand2: Strand, is_expected_to
     print(f'{format_strand_name(strand1.name)} | {format_strand_name(strand2.name)} | binding: {binding_val: {binding_width}.{binding_percision}} {is_expected_to_bind_str}')
 
 
+# Define physical model
+model = Model(material='dna', celsius=37)
+
+
+def calculate_and_print_pairs_and_mfe(strands: Iterable[Strand]) -> None:
+    nupack_strands = [NupackStrand(s.sequence, name=s.name) for s in strands]
+
+    # Define the complex of interest
+    strand_complex = Complex(nupack_strands)
+
+    # Define the complex set to contain only one complex
+    complex_set = ComplexSet(strands=nupack_strands, complexes=SetSpec(max_size=0, include=[strand_complex]))
+
+    # Analyze the complex
+    # Calculate pfunc, pairs, mfe
+    result = complex_analysis(complex_set, compute=['pfunc', 'pairs', 'mfe'], model=model)
+
+    complex_result = result[strand_complex]
+    print(f'\nMFE proxy structure for complex {strand_complex.name}:')
+    for i, s in enumerate(complex_result.mfe):
+        print('    %2d: %s (%.2f kcal/mol)' % (i, s.structure, s.energy))
+
+    print(f'\nPair probabilities for complex {strand_complex.name}:')
+    arr = complex_result.pairs.to_array()
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            p = arr[i][j]
+            if p > 0.01:
+                print(f'    P({i}, {j}) = {p}')
+
+
 def main():
     parser = argparse.ArgumentParser(description='Runs analysis of square root circuit DNA sequences.')
     parser.add_argument('filename', help='The sequence.txt file.')
@@ -156,13 +189,12 @@ def main():
     with open(filename) as f:
         for line in f:
             process_line(line)
+    global longest_strand_name_length
+    longest_strand_name_length = reduce(lambda acc, cur: max(acc, len(cur.name)), strands(), 0)
 
     print(f'Done processing {filename}')
 
     print('\nCalculating pfunc...')
-    global longest_strand_name_length
-    longest_strand_name_length = reduce(lambda acc, cur: max(acc, len(cur.name)), strands(), 0)
-
     pfunc4_width = 5
     pfunc4_percision = 5
     for strand in strands():
@@ -172,6 +204,36 @@ def main():
     print('\nCalculating binding between pairs of signal strands (including fuel)...')
     for strand1, strand2 in combinations(chain(signal_strands, fuel_strands), 2):
         calculate_and_print_binding(strand1, strand2)
+
+    gate_to_reporter_bottom_strand: Dict[int, SignalStrand] = {s.gate: s for s in reporter_bottom_strands}
+
+    print('\nCalculating base-pairing probabilities and MFE for each input:gate complex')
+    gate_5p_to_signal_strand: Dict[str, SignalStrand] = {s.gate_5p: s for s in signal_strands}
+    for gate_base_strand in gate_base_strands:
+        assert gate_base_strand.gate in gate_5p_to_signal_strand
+        output_strand = gate_5p_to_signal_strand[gate_base_strand.gate]
+        calculate_and_print_pairs_and_mfe([output_strand, gate_base_strand])
+
+    print('\nCalculating base-pairing probabilities and MFE for each gate:output complex')
+    gate_3p_to_signal_strand: Dict[str, SignalStrand] = {s.gate_3p: s for s in signal_strands}
+    for gate_base_strand in gate_base_strands:
+        # TODO: remove this if statement
+        if gate_base_strand.gate in gate_3p_to_signal_strand:
+            assert gate_base_strand.gate in gate_3p_to_signal_strand
+            input_strand = gate_3p_to_signal_strand[gate_base_strand.gate]
+            calculate_and_print_pairs_and_mfe([input_strand, gate_base_strand])
+        else:
+            # TODO: remove this else statement
+            # this assertion checks that the only cause of not finding a gate base strand's gate
+            # in signal strand map is that it belongs to a reporter bottom strand
+            assert gate_base_strand.gate in gate_to_reporter_bottom_strand
+
+    print('\nCalculating base-pairing probabilities and MFE for each gate:fuel complex')
+    gate_to_gate_base_strand: Dict[str, GateBaseStrand] = {s.gate: s for s in gate_base_strands}
+    for fuel_strand in fuel_strands:
+        assert fuel_strand.gate_3p in gate_to_gate_base_strand
+        gate_base_strand = gate_to_gate_base_strand[fuel_strand.gate_3p]
+        calculate_and_print_pairs_and_mfe([fuel_strand, gate_base_strand])
 
     # print('\nCalculating binding between same strand type...')
     # for itr in strand_iterators:
