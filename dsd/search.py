3863fa728f502863d737e95d9c43365d60fad497
David Doty <doty@ucdavis.edu>
2021-07-05 14:12:31 -0700

    added NUPACK license text to source code comments in preparation for eventual PyPI distribution

--- dsd/search.py
+++ dsd/search.py
@@ -3,6 +3,46 @@ Stochastic local search for finding DNA sequences to assign to
 :any:`Domain`'s in a :any:`Design` to satisfy all :any:`Constraint`'s.
 """
 
+# Since dsd is distributed with NUPACK, we include the following license
+# agreement as required by NUPACK. (http://www.nupack.org/downloads/register)
+#
+# NUPACK Software License Agreement for Non-Commercial Academic Use and
+# Redistribution
+# Copyright © 2021 California Institute of Technology. All rights reserved.
+#
+# Use and redistribution in source form and/or binary form, with or without
+# modification, are permitted for non-commercial academic purposes only,
+# provided that the following conditions are met:
+#
+# Redistributions in source form must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# provided with the distribution.
+#
+# Web applications that use the software in source form or binary form must
+# reproduce the above copyright notice, this list of conditions and the
+# following disclaimer in online documentation provided with the web
+# application.
+#
+# Neither the name of the copyright holder nor the names of its contributors
+# may be used to endorse or promote derivative works without specific prior
+# written permission.
+#
+# Disclaimer
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed.  In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
+
 import math
 import itertools
 import os
