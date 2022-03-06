414ad1460e2c54e9332a73ddd27d098a10321030
David Doty <doty@ucdavis.edu>
2022-03-06 09:21:44 +0000

    Update search.py

--- dsd/search.py
+++ dsd/search.py
@@ -1110,7 +1110,10 @@ def create_report(design: dc.Design, constraints: Iterable[Constraint]) -> str:
     from a design.json file writte as part of a call to :meth:`search_for_dna_sequences`.
 
     The report is the same format as written to the reports generated when calling
-    :meth:`search_for_dna_sequences`
+    :meth:`search_for_dna_sequences`.
+    Unfortunately this means it suffers the limitation that it currently only prints a summary
+    for *violations* of constraints;
+    see https://github.com/UC-Davis-molecular-computing/dsd/issues/134
 
     :param design:
         the :any:`constraints.Design`, with sequences assigned to all :any:`Domain`'s
