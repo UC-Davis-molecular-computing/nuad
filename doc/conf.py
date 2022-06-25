cf4b3639b445461272936f0171895b458114abda
David Doty <doty@ucdavis.edu>
2022-06-24 17:38:26 -0700

    bumped version

--- doc/conf.py
+++ doc/conf.py
@@ -29,7 +29,7 @@ copyright = '2020, David Doty and Damien Woods'
 author = 'David Doty and Damien Woods'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.3'
+release = '0.1.4'
 # version = __version__
 # release = __version__
 
