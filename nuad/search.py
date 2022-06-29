b0bc0f423992274d04eb0d91a8ab1b09bf859916
Constantine Evans <const@costi.eu>
2022-06-29 17:56:51 +0100

    Create directory before opening logs (fixes #182)

--- nuad/search.py
+++ nuad/search.py
@@ -1108,10 +1108,10 @@ def _setup_directories(params: SearchParameters) -> _Directories:
     out_directory = params.out_directory
     if out_directory is None:
         out_directory = default_output_directory()
+    if not os.path.exists(out_directory):
+        os.makedirs(out_directory)
     directories = _Directories(out=out_directory, debug=params.debug_log_file,
                                info=params.info_log_file)
-    if not os.path.exists(directories.out):
-        os.makedirs(directories.out)
     if not params.restart:
         _clear_directory(directories.out, params.force_overwrite)
     for subdir in directories.all_subdirectories(params):
