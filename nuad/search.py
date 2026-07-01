c42062e1682b1711b99ee6f64ebe3d56a04451fa
mHbNYT <mahboob.e78@gmail.com>
2026-07-01 13:35:27 +0330

    modified _assignable_domains_in_part (independent to assignable)

--- nuad/search.py
+++ nuad/search.py
@@ -407,16 +407,16 @@ def _assignable_domains_in_part(part: DesignPart, exclude_fixed: bool) -> Tuple[
         )
 
     # Convert direct domains to independent domains.
-    # If multiple dependent domains map to the same indepedent domain d_i, only add d_i once
+    # If multiple dependent domains map to the same assignable domain d_i, only add d_i once
     assignable_domains = []
+    assignable_domains_connected_to_domain = []
     for domain in domains:
         if domain.type == DomainType.ASSIGNABLE:
             assignable_domains_connected_to_domain = [domain]
         elif domain.type == DomainType.LOCKED:
             assignable_domains_connected_to_domain = domain.assignable_ancestors_or_descendants()
 
-        if (domain.type == DomainType.ASSIGNABLE or domain.type == DomainType.LOCKED) and assignable_domains_connected_to_domain not in assignable_domains:
-            assignable_domains.extend(assignable_domains_connected_to_domain)
+        assignable_domains = list(set(assignable_domains + assignable_domains_connected_to_domain))
 
     return tuple(assignable_domains)
 
